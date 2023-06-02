50efb8c5d086a28be48594de363cfe652e2d222a
Kevin <kevinstewartmercurio@gmail.com>
2023-06-02 13:06:27 -0400

    responsive design tweaks

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -27,19 +27,21 @@ export function Schedule(props: {masterSchedule: [Date, Event[]][], scheduleGene
                 <table className="text-gray-300 w-5/6 text-sm md:text-base font-normal">
                     <thead className="text-left">
                         <tr>
-                            <th className="pl-3">
+                            <th className="w-1/5 sm:w-1/4 px-3">
                                 Date
                             </th>
-                            <th>
+                            <th className="pr-3">
                                 <div className="w-full flex flex-row">
                                     <div className="w-1/2">
                                         Player(s)
                                     </div>
-                                    <div className="w-2/5">
-                                        Location
-                                    </div>
-                                    <div className="w-[1/10]">
-                                        Field
+                                    <div className="w-1/2 flex flex-row">
+                                        <div className="w-4/5">
+                                            Location
+                                        </div>
+                                        <div className="hidden md:block">
+                                            Field
+                                        </div>
                                     </div>
                                 </div>
                             </th>
@@ -55,10 +57,10 @@ export function Schedule(props: {masterSchedule: [Date, Event[]][], scheduleGene
 
                             return (
                                 <tr key={outerIndex} className={`${conditionalBackground}`}>
-                                    <td className={`w-1/4 pl-3 py-3 ${conditionalRoundedLeft}`}>
+                                    <td className={`pl-3 py-3 ${conditionalRoundedLeft}`}>
                                         {dateTxt}
                                     </td>
-                                    <td className={`py-3 ${conditionalRoundedRight}`}>
+                                    <td className={`pr-3 py-3 ${conditionalRoundedRight}`}>
                                         <div className="flex flex-col">
                                             {outerItem[1].map((innerItem: Event, innerIndex: number) => {
                                                 const conditionalPLDWidth = `${innerItem.location === "PLD (Parking Lot Duty)" ? "w-full" : "w-4/5"}`
