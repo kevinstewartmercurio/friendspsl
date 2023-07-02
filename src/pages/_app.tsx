ca6070094f3c935a0fa8789d6aa9156ee6571a64
Kevin <kevinstewartmercurio@gmail.com>
2023-07-02 19:45:26 -0400

    working on adding leagues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>FriendsPSL</title>
         <link rel="icon" href="/smile.png" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1446470018172212" crossOrigin="anonymous"></script>
       </Head>
       <Component {...pageProps} />
       <Analytics />
