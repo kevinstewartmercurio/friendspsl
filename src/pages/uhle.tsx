5e4a9c1ade4c5a280ce439e4f59fd9854d89f8c0
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2023-08-03 11:49:56 -0400

    splitting server side code for pulling uhle players

--- src/pages/uhle.tsx
+++ src/pages/uhle.tsx
@@ -13,6 +13,8 @@ export default function Uhle() {
     const contentRef = useRef<HTMLDivElement>(null)
     const [contentHeight, setContentHeight] = useState(0)
 
+    const [pulling, setPulling] = useState<boolean[]>([false, false])
+
     const [popup, setPopup] = useState<string>("")
     const [masterSchedule, setMasterSchedule] = useState<[Date, Event[]][]>([])
     const [scheduleGenerated, setScheduleGenerated] = useState<boolean>(false)
@@ -20,17 +22,44 @@ export default function Uhle() {
     const [errorType, setErrorType] = useState<string>("")
 
     useEffect(() => {
-        const pullPlayers = async () => {
-            await fetch("/api/pullPlayers", {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({league: "uhle"})
-            })
-                .catch((error) => console.error(error))
-        }
+        if (pulling[0] === false) {
+            console.log("pulling 1")
+            const pullPlayers1 = async () => {
+                await fetch("/api/pullUhlePlayers", {
+                    method: "POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify({start: 1, end: 5})
+                })
+                    .then((res) => {
+                        if (res.ok) {
+                            setPulling((prev) => [true, prev[1]])
+                        }
+                    })
+            }
 
-        pullPlayers()
+            pullPlayers1()
+        }
     }, [])
+
+    useEffect(() => {
+        if (pulling[0] === true) {
+            console.log("pulling 2")
+            const pullPlayers2 = async () => {
+                await fetch("/api/pullUhlePlayers", {
+                    method: "POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify({start: 6, end: 10})
+                })
+                    .then((res) => {
+                        if (res.ok) {
+                            setPulling((prev) => [prev[0], true])
+                        }
+                    })
+            }
+
+            pullPlayers2()
+        }
+    }, [pulling[0]])
 
     useEffect(() => {
         if (masterSchedule.length > 0) {
